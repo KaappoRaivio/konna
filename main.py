1788784252db5480451b8647854b58c804299239
NoSocksForYou <kaappo.raivio@gmail.com>
2018-04-02 09:38:58 +0300

    it can now jump as well

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import konna
 from graphics import Point, Rectangle
 
-a = konna.Konna(no_bounds=True, framerate=1000, window_dim_x=1000, window_dim_y=1000)
+a = konna.Konna(no_bounds=True, framerate=100, window_dim_x=1000, window_dim_y=1000)
 
 # print(a.__framerate, 'asd')
 
@@ -14,24 +14,30 @@ rect.setFill('black')
 a.penDown()
 
 a.orientation = 0
-a.line_color = 'red'
+a.line_color = 'blue'
 a.line_width = 1
 
 
-värit = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black']
+värit = ['red', 'orange', 'yellow', 'green', 'blue', 'red']#, 'purple']#, 'black']
 
 i = 10
 
-span = 1000
+span = 500
 
 while KeyboardInterrupt:
     i += 1
-    a.orientation += 91
+    a.orientation += 29
 
     # a.line_color = värit[i % len(värit)]
 
     a.move(i)
 
-    for c in range(len(värit)):
-        if (c - 1) * span < c * (i % span) < c * span:
-            a.line_color = värit[c]
+    # for c in range(len(värit)):
+    #     if (c - 1) * span < c * (i % span) < c * span:
+    #         a.line_color = värit[c]
+
+    if i % 4 == 0:
+        a.jump(10, 10)
+
+    if i % 3 == 0:
+        a.jump(-10, -10)
